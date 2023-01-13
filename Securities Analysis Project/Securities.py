18a7bc66168dea97841d251500d2e9688020c143
Lester-723 <119092044+Lester-723@users.noreply.github.com>
2023-01-13 22:27:47 +0530

    new Python notebook added

--- Securities Analysis Project/Securities.py
+++ Securities Analysis Project/Securities.py
@@ -11,22 +11,25 @@ Requires:
     - yfinance
 """
 
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot
+from pandas_datareader import data as pdr
+import yfinance as yf
 
 class Stock_analysis():
-    Stock_table = pd.DataFrame()
+    Stock_Data = pd.DataFrame()
         
     def __init__(self,tickers : list,start_date):
             self.tickers = tickers
-            self.start_date = start_date
-           
-        
+            self.start_date = start_date  
+
 
     def download_stocks(self, column_name : str):
             self.column_name = column_name
             yf.pdr_override()
-            for ticks in self.tickers:
-                Stock_analysis.Stock_table[ticks] = pdr.DataReader(ticks, self.start_date)[column_name]
-            return Stock_analysis.Stock_table
+            Stock_analysis.Stock_Data[ticks] = [pdr.DataReader(ticks, self.start_date)[column_name] for ticks in self.tickers]
+            return Stock_analysis.Stock_Data
 
         
 
@@ -34,16 +37,20 @@ class Stock_analysis():
         
             self.return_type = return_type
             if return_type == "Log":
-                returns_data = np.log(Stock_analysis.Stock_table/Stock_analysis.Stock_table.shift(1))
+                returns_data = np.log(Stock_analysis.Stock_Data/Stock_analysis.Stock_Data.shift(1))
             if return_type == "Simple": 
-                returns_data = (Stock_analysis.Stock_table/Stock_analysis.Stock_table.shift(1))-1
+                returns_data = (Stock_analysis.Stock_Data/Stock_analysis.Stock_Data.shift(1))-1
             return returns_data
 
     def normalization(self):
-            normalized_data = (Stock_analysis.Stock_table/Stock_analysis.Stock_table.iloc[0]*100)
+            normalized_data = (Stock_analysis.Stock_Data/Stock_analysis.Stock_Data.iloc[0]*100)
             normalized_figure = normalized_data.plot(figsize = (15,6))
             plt.title("Growth Comparison of Stocks")
             return normalized_figure
+    
+    def individual_return():
+            individual_return = self.return_calculator(self.return_type).mean()*250
+            print(f"The {self.return_type} return of {ticks for ticks in self.tickers} is {round(individual_return,2)} %")
         
 
     def portfolio_return(self,weights = list):
@@ -51,7 +58,7 @@ class Stock_analysis():
             weights = np.array(weights)
             if sum(weights)!= 1:
                 raise ValueError(f"Sum of the weight of the porfolio should be 1,The sum of your weights are {sum(weights)}")
-                if len(weights) != Stock_analysis.Stock_table.shape[1]:
+                if len(weights) != Stock_analysis.Stock_Data.shape[1]:
                     sys.exit("The weights of individual security does not match the no. of securities")
             annual_returns = np.dot(self.return_calculator(self.return_type).mean()*250, weights)
             print(f"The annual returns of the portfolio is {round(annual_returns, 2)}% ")
